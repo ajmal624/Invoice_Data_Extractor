503c960fbefe9cde916e0a905cc56b489a896c48
Mohammed_Ajmal <mohammedajmalajmal2003@gmail.com>
2025-11-05 16:20:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from datetime import datetime, timedelta
 
 # ========= CONFIG =========
 load_dotenv()
-OPENAI_API_KEY = st.secrets("OPENAI_API_KEY")
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 if not OPENAI_API_KEY:
     st.error("❌ OPENAI_API_KEY not found in .env file")
     st.stop()
@@ -284,5 +284,6 @@ if uploaded_file:
         except Exception as e:
 
             st.error(f"❌ Error: {e}")
+
 
 
